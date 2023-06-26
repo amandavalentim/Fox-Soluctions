41026691104e08c4af6b956f9e289d530ab6f22b
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-06-26 16:01:53 -0300

    Salvando

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -10,6 +10,7 @@ const HeadComponent = styled.head`
     justify-content: space-between;
     align-items: center;
     padding: 20px;
+    position: relative;
     .nameLogo {
         color: white;
     }
--- src/components/ulix/background/background.tsx
+++ src/components/ulix/background/background.tsx
@@ -6,12 +6,63 @@ const BackgroundComponent = styled.body`
     background-color: #0A53B5;
 `;
 
+const Imgs1 = styled.div`
+    position: absolute;
+    top: 390px;
+`;
+const Imgs2 = styled.div`
+    position: absolute;
+    top: 95px;
+    left: -1px;
+`;
+const Imgs3 = styled.div`
+    position: absolute;
+    top: 80px;
+    left: 755px;
+`;
+const Imgs4 = styled.div`
+    position: absolute;
+    top: 270px;
+    left: 295px;
+`;
+const Imgs5 = styled.div`
+    position: absolute;
+    top: 270px;
+    left: 295px;
+`;
+const Imgs6 = styled.div`
+    position: absolute;
+    top: 270px;
+    left: 295px;
+`;
+const Imgs7 = styled.div`
+    position: absolute;
+    top: 270px;
+    left: 295px;
+`;
+const Imgs8 = styled.div`
+    position: absolute;
+    top: 270px;
+    left: 295px;
+`;
+const Imgs9 = styled.div`
+    position: absolute;
+    top: 270px;
+    left: 295px;
+`;
+
 const Background = function() {
     return(
         <BackgroundComponent>
-            <div>
-                <Image src="/images/Group 77.png" width={81} height={104} alt="background" />
-            </div>
+            <Imgs1>
+                <Image src="/images/Ellipse 1.png" width={310} height={225} alt='img-1'/>
+            </Imgs1>
+            <Imgs2>
+                <Image src="/images/Rectangle 20.png" width={165} height={200} alt='img-2'/>
+            </Imgs2>
+            <Imgs3>
+                <Image src="/images/Ellipse 5.png" width={200} height={115} alt='img-3' />
+            </Imgs3>
         </BackgroundComponent>
     )
 }
