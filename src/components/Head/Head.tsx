40fed137739f6c4e7c66f2cf3aeee8fba64da1e9
Gustavo Otacilio <gustavootaciliodossantos@gmail.com>
2023-06-27 10:52:25 -0300

    atualizando

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const HeadComponent = styled.head`
     background: linear-gradient(180deg, #07B3FD 0%, #0081E8 100%);
-    height: 190px;
+    height: 150px;
     width: 100%;
     display: flex;
     justify-content: space-between;
@@ -13,9 +13,7 @@ const HeadComponent = styled.head`
     .nameLogo {
         color: white;
     }
-    .image {
-        
-    }
+
     @media (min-width: 768px){
         height: 172px;
     }
