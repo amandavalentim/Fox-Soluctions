77be8e83e06e36a3820241b2dfaf6cff745ce79b
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-06-27 10:08:34 -0300

    Salvando

--- src/components/ulix/background/background.tsx
+++ src/components/ulix/background/background.tsx
@@ -4,65 +4,44 @@ import Image from 'next/image';
 
 const BackgroundComponent = styled.body`
     background-color: #0A53B5;
+    #img1 {
+        position: absolute;
+        top: 390px;
+    }
+    #img2 {
+        position: absolute;
+        top: 95px;
+        left: -1px;
+    }
+    #img3 {
+        position: absolute;
+        top: 130px;
+        left: 735px;
+    }
+    #img4 {
+        position: absolute;
+        top: 1355px;
+        left: 1355px;
+    }
 `;
 
-const Imgs1 = styled.div`
-    position: absolute;
-    top: 390px;
-`;
-const Imgs2 = styled.div`
-    position: absolute;
-    top: 95px;
-    left: -1px;
-`;
-const Imgs3 = styled.div`
-    position: absolute;
-    top: 80px;
-    left: 755px;
-`;
-const Imgs4 = styled.div`
-    position: absolute;
-    top: 270px;
-    left: 295px;
-`;
-const Imgs5 = styled.div`
-    position: absolute;
-    top: 270px;
-    left: 295px;
-`;
-const Imgs6 = styled.div`
-    position: absolute;
-    top: 270px;
-    left: 295px;
-`;
-const Imgs7 = styled.div`
-    position: absolute;
-    top: 270px;
-    left: 295px;
-`;
-const Imgs8 = styled.div`
-    position: absolute;
-    top: 270px;
-    left: 295px;
-`;
-const Imgs9 = styled.div`
-    position: absolute;
-    top: 270px;
-    left: 295px;
-`;
+
 
 const Background = function() {
     return(
         <BackgroundComponent>
-            <Imgs1>
-                <Image src="/images/Ellipse 1.png" width={310} height={225} alt='img-1'/>
-            </Imgs1>
-            <Imgs2>
-                <Image src="/images/Rectangle 20.png" width={165} height={200} alt='img-2'/>
-            </Imgs2>
-            <Imgs3>
-                <Image src="/images/Ellipse 5.png" width={200} height={115} alt='img-3' />
-            </Imgs3>
+                <div id='img1'>
+                    <Image src="/images/Ellipse-1.png" width={310} height={225} alt='img-1'/>
+                </div>
+                <div id='img2'>
+                    <Image src="/images/Rectangle 20.png" width={165} height={200} alt='img-2'/>
+                </div>
+                <div id='img3'>
+                    <Image src="/images/Ellipse 5-8.png" width={200} height={65} alt='img-3.8'/>
+                </div>
+                <div id='img4'>
+                    <Image src="/images/header.png" width={270} height={225} alt='img-4'/>
+                </div>
         </BackgroundComponent>
     )
 }
