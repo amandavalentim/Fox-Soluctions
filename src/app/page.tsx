61187714833b2777469dea961aa42a05dee6db1c
Gustavo Otacilio <gustavootaciliodossantos@gmail.com>
2023-06-27 15:30:07 -0300

    form
    iniciando a tela de formulário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Background from '@/components/ulix/background/background'
 import Head  from '../components/Head/Head'
 import Footer  from '../components/Footer/Footer'
+import Form from '../components/Forms/form'
 
 export default function Home() {
   return (
     <main>
       <Head/>
       <Background/>
+      <Form/>
       <Footer/>
     </main>
 
