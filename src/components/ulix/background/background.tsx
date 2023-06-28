057ddfda5b1850fe25c525245edf1bd49287b483
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-06-28 11:31:26 -0300

    Atualizando

--- src/components/ulix/background/background.tsx
+++ src/components/ulix/background/background.tsx
@@ -151,7 +151,7 @@ const BackgroundComponent = styled.body`
     }
     }*/
 
-    @media ((max-width: 1114px)){
+    @media ((max-width: 1130px)){
         #img1 {
         position: absolute;
         top: 370px;
