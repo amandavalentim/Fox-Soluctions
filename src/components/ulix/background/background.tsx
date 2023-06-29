cc939d4ff9cf03a098a635ca04ab8d5b768b9253
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-06-29 09:28:17 -0300

    Atualizando

--- src/components/ulix/background/background.tsx
+++ src/components/ulix/background/background.tsx
@@ -7,7 +7,14 @@ const BackgroundComponent = styled.body`
     position: relative;
     width: 100%;
     height: 1200px;
+    .hide-on-small-desktops {
+        display: flex;
+        justify-content: space-between;
+    }
     .show-in-small-desktops {
+        display: none;
+    }
+    .desktops-1024-1100-screens {
         display: none;
     }
     #img1 {
@@ -69,13 +76,16 @@ const BackgroundComponent = styled.body`
         top: 805px;
     }
 
-    /*@media ((min-width: 1024px) and (max-width: 1100px)){
+    @media ((min-width: 1024px) and (max-width: 1100px)){
         .hide-on-small-desktops {
-        display: flex;
-        justify-content: space-between;
+        display: none;
     }
     .show-in-small-desktops {
         display: none;
+    }
+    .desktops-1024-1100-screens {
+        display: flex;
+        justify-content: space-between;
     }
     #img1 {
         position: absolute;
@@ -159,7 +169,7 @@ const BackgroundComponent = styled.body`
         width: 100px;
         height: 100px;
     }
-    }*/
+    }
 
     @media ((min-width: 1115px) and (max-width: 1200px)){
         .hide-on-small-desktops {
@@ -169,65 +179,68 @@ const BackgroundComponent = styled.body`
         display: flex;
         justify-content: space-between;
     }
+    .desktops-1024-1100-screens {
+        display: none;
+    }
         #img1 {
         position: absolute;
         top: 370px;
-        width: 280px;
-        height: 190px;
+        width: 250px;
+        height: 170px;
         }
     #img2 {
         position: absolute;
         top: -25px;
         left: -1px;
-        width: 130px;
-        height: 180px;
+        width: 110px;
+        height: 160px;
     }
     #img3 {   
         position: absolute;
         top: -5px;
-        left: 630px;
-        width: 200px;
-        height: 70px;
+        left: 485px;
+        width: 180px;
+        height: 50px;
     }
     #img4 {
         position: absolute;
         top: -5px;
-        left: 1153px;
+        left: 953px;
         width: 250px;
         height: 190px;
     }
     #img5 {
         position: absolute;
         top: 190px;
-        left: 270px;
+        left: 170px;
         width: 250px;
         height: 170px;
     }
     #img6 {
         position: absolute;
         top: 200px;
-        left: 815px;
+        left: 615px;
         width: 350px;
         height: 155px;
     }
     #img7 {
         position: absolute;
         top: 415px;
-        left: 1013px;
+        left: 805px;
         width: 165px;
         height: 80px;
     }
     #img8 {
         position: absolute;
         top: 715px;
-        left: 1100px;
+        left: 873px;
         width: 250px;
         height: 230px;
     }
     #img9 {
         position: absolute;
         top: 680px;
-        left: 545px;
+        left: 445px;
         width: 350px;
         height: 360px;
     }
@@ -246,7 +259,7 @@ const BackgroundComponent = styled.body`
     }
     #img12 {
         position: absolute;
-        left: 650px;
+        left: 550px;
         top: 790px;
         width: 140px;
         height: 140px;
@@ -273,6 +286,20 @@ const Background = function() {
             </nav>
 
             <nav className='show-in-small-desktops'>
+                <Image src="/images/Ellipse-1.png" width={310} height={225} alt='img-1' id='img1'/>
+                <Image src="/images/Rectangle-20.png" width={165} height={200} alt='img-2' id='img2'/>
+                <Image src="/images/Ellipse-5.png" width={200} height={65} alt='img-3.8' id='img3'/>
+                <Image src="/images/header.png" width={270} height={225} alt='img-4' id='img4'/>
+                <Image src="/images/Ellipse-2.png" width={265} height={200} alt='img-5' id='img5'/>
+                <Image src="/images/Ellipse-3.png" width={400} height={200} alt='img-6' id='img6'/>
+                <Image src="/images/Ellipse-8.png" width={200} height={95} alt='img-7' id='img7'/>
+                <Image src="/images/Ellipse-4.png" width={265} height={200} alt='img-8' id='img8'/>
+                <Image src="/images/Ellipse-7.png" width={350} height={425} alt='img-9' id='img9'/>
+                <Image src="/images/Ellipse-6-1.png" width={450} height={180} alt='img-10' id='img10'/>
+                <Image src="/images/Stella.png" width={130} height={130} alt='img-12' id='img12'/>
+            </nav>
+
+            <nav className='desktops-1024-1100-screens'>
                 <Image src="/images/Ellipse-1.png" width={310} height={225} alt='img-1' id='img1'/>
                 <Image src="/images/Rectangle-20.png" width={165} height={200} alt='img-2' id='img2'/>
                 <Image src="/images/Ellipse-5.png" width={200} height={65} alt='img-3.8' id='img3'/>
