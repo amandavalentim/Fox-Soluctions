499747c6e0b37058e990bbc1f36459c6300e227c
Gustavo Otacilio <gustavootaciliodossantos@gmail.com>
2023-07-03 12:25:08 -0300

    finalização de formulário mobile 320x425px

--- src/components/Forms/form.tsx
+++ src/components/Forms/form.tsx
@@ -44,6 +44,8 @@ const FormComponent = styled.div`
            div {
                width: 100%;
                 .submit {
+                    position: absolute;
+                    left: 45px;
                     width: 100%;
                     height: 40px;
                     margin-top: 30px;
@@ -119,6 +121,8 @@ const FormComponent = styled.div`
            div {
                width: 100%;
                 .submit {
+                    position: absolute;
+                    left: 45px;
                     width: 100%;
                     height: 40px;
                     margin-top: 30px;
@@ -203,6 +207,8 @@ const FormComponent = styled.div`
            div {
                width: 100%;
                 .submit {
+                    position: absolute;
+                    left: 45px;
                     width: 100%;
                     height: 40px;
                     margin-top: 30px;
@@ -251,83 +257,85 @@ const FormComponent = styled.div`
             display: flex;
         }
         .box-container-mobile {
-        border-radius: 19px;
-        background: linear-gradient(180deg, #07B3FD 0%, #0A53B5 100%);
-        width: 528px;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        margin-left: 410px;
-        margin-top: -190px;
-        form {
-            #title-mobile {
-                color: #FFFF;
-                text-align: center;
-                margin: 30px;
-                font-family: 'Poppins-Bold';
-            }
-            .login-mobile {
-                margin: 50px;
-                display: flex;
-                input {
-                    border: none;
-                    width: 300px;
-                    border-bottom: 1px solid #FFFF;
-                    background-color: rgba(7, 179, 253, 0);
-                    justify-content: center;
-                    align-items: center;
-                    padding-bottom: 20px;
-                }
-
-                input::placeholder{
-                    font-size: 15px;
-                    font-family: 'Poppins-Medium';
+            border-radius: 19px;
+            width: 528px;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            margin-left: -12px;
+            margin-top: -350px;
+            form {
+                #title-mobile {
                     color: #FFFF;
+                    text-align: center;
+                    margin: 30px;
+                    font-family: 'Poppins-Bold';
                 }
-            }
-           div {
-               width: 100%;
-                .submit-mobile {
+                .login-mobile {
+                        margin: 50px;
+                        display: flex;
+                    input {
+                        border: none;
+                        width: 250px;
+                        border-bottom: 1px solid #FFFF;
+                        background-color: rgba(7, 179, 253, 0);
+                        justify-content: center;
+                        align-items: center;
+                        padding-bottom: 20px;
+                    }
+
+                    input::placeholder{
+                        font-size: 10px;
+                        font-family: 'Poppins-Medium';
+                        color: #FFFF;
+                    }
+                }
+            div {
                     width: 100%;
-                    height: 40px;
-                    margin-top: 30px;
-                    border-radius: 20px;
-                    border: none;
-                    background-color: #93B413;
-                    justify-content: center;
-                    align-items: center;
-                    color: #FFFF;
-                    font-family: 'Poppins-Medium';
-                    font-size: 20px; 
-                    cursor: pointer;
+                    .submit-mobile {
+                        position: absolute;
+                        left: 45px;
+                        width: 70%;
+                        height: 40px;
+                        margin-top: 30px;
+                        border-radius: 20px;
+                        border: none;
+                        background-color: #93B413;
+                        justify-content: center;
+                        align-items: center;
+                        color: #FFFF;
+                        font-family: 'Poppins-Medium';
+                        font-size: 15px; 
+                        cursor: pointer;
+                    }
                 }
-            }
 
-            .forget-mobile {
-                color: #F47216;
-                font-size: 24px;
-                font-family: Poppins;
-                font-style: normal;
-                font-weight: 500;
-                line-height: normal;
-                text-align: center;
-                margin-bottom: 10px;
-                cursor: pointer;
-            } 
-            .create_account-mobile {
-                color: #FFF;
-                font-size: 24px;
-                font-family: Poppins;
-                font-style: normal;
-                font-weight: 500;
-                line-height: normal;
-                text-align: center;
-                cursor: pointer;
-                span {
+                .forget-mobile {
                     color: #F47216;
-                }
-            } 
-        }
+                    font-size: 15px;
+                    font-family: Poppins;
+                    font-style: normal;
+                    font-weight: 500;
+                    line-height: normal;
+                    text-align: center;
+                    margin-bottom: 10px;
+                    cursor: pointer;
+                    
+                } 
+                .create_account-mobile {
+                    color: #FFF;
+                    font-size: 15px;
+                    font-family: Poppins;
+                    font-style: normal;
+                    font-weight: 500;
+                    line-height: normal;
+                    text-align: center;
+                    cursor: pointer;
+                    span {
+                        color: #F47216;
+                    }
+                } 
+            }
     }
 }
 `;
@@ -365,7 +373,7 @@ const Form = function(){
             <nav className='show-on-mobile'>
                 <div className='box-container-mobile'>
                     <form>
-                        <h1 id='title-mobile'>Login</h1>
+                        <h1 id='title-mobile'></h1>
                         <div className='login-mobile'>
                             <input type="email" name="e-mail" id="e-mail" placeholder='E-mail'/>
                         </div>
