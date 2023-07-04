8d5bad3602a4590e030075d39e9c300ff229caea
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-07-04 12:39:25 -0300

    Atualizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Background from '@/components/ulix/background/background'
 import Head  from '../components/Head/Head'
 import Footer  from '../components/Footer/Footer'
 import Form from '../components/Forms/form'
-import Body from '@/components/body'
 
 export default function Home() {
   return (
