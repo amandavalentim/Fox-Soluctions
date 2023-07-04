d5064dd4ed68b684fa24c506e388efbcae0a367a
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-07-04 12:19:47 -0300

    Atualizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
       <Background/>
       <Footer/>
       <Form/>
-      <Body/>
     </main>
 
   )
--- src/components/ulix/background/background.tsx
+++ src/components/ulix/background/background.tsx
@@ -1,21 +1,26 @@
 'use client';
 import styled from '@emotion/styled';
-import Image from 'next/image';
-
 const BackgroundComponent = styled.html`
     background-image: url("/images/background2.svg");
     background-repeat: no-repeat;
-    background-size: 100% ;
+    background-size: cover ;
+    height: 100vh;
 
     @media((min-width: 1024px) and (max-width: 1100px)){
-    background-image: url("/images/background1-2.svg");
+    background-image: url("/images/background1.svg");
     background-color: #0A53B5;
     background-position-y: 25px;
     }
 
-    @media((min-width: 320px) and (max-width: 640px)) {
-        background: linear-gradient(180deg, #0A53B5 0%, #07B3FD 100%);
-        height: 1200px;
+    @media((min-width: 1360px) and (max-width: 1460px)){
+    background-image: url("/images/background3.svg");
+    background-color: #0A53B5;
+    background-position-y: 25px;
+    }
+    
+    @media((min-width: 320px) and (max-width: 400px)){
+    background: linear-gradient(180deg, #0A53B5 0%, #07B3FD 100%);
+    height: 1200px;
     }
 `;
 
