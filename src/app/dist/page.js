3228819e07a0539783b0813075b0251f7123ce63
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-07-05 09:17:18 -0300

    Atualizando

--- src/app/dist/page.js
+++ src/app/dist/page.js
@@ -1,5 +1,6 @@
 "use strict";
 exports.__esModule = true;
+var background_1 = require("@/components/ulix/background/background");
 var Head_1 = require("../components/Head/Head");
 var Footer_1 = require("../components/Footer/Footer");
 var form_1 = require("../components/Forms/form");
@@ -7,6 +8,7 @@ var body_1 = require("@/components/body");
 function Home() {
     return (React.createElement("main", null,
         React.createElement(Head_1["default"], null),
+        React.createElement(background_1["default"], null),
         React.createElement(Footer_1["default"], null),
         React.createElement(body_1["default"], null),
         React.createElement(form_1["default"], null)));
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main>
       <Head/>
+      <Background/>
       <Footer/>
       <Body/>
       <Form/>
