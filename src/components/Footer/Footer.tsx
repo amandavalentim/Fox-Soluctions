50a7e8abaaf5a3f3d592bc6ec9a367ad3640ea8c
Amanda Vitoria Valentim Dos Anjos <amanda.anjos@tecno.com.br>
2023-07-06 12:24:29 -0300

    alterando conteúdo da footer - mobile

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,167 +3,47 @@ import styled from '@emotion/styled';
 import { FaFacebookSquare, FaInstagram, FaTwitterSquare, FaGithubSquare} from "react-icons/fa";
 
 const FooterComponent = styled.footer`
-    background: linear-gradient(180deg, rgba(7, 179, 253, 1), rgba(7, 179, 253, 1));
-    height: 400px;
-    width: 100%;
-    position: relative;
-    .show-on-mobile{
-        display: flex;
-        justify-content: space-between;
-    }
+    background: rgba(7, 179, 253, 1);
+    box-sizing: border-box;
     .container-center{
-        margin: auto;
-        padding: auto;
-        .box-mobile{
-            width: 250px;
-            height: 78px;
-            border-radius: 5px;
-            border: 3px solid #93B413;
-            background: #93B413;
-            margin: 40px;
-            box-sizing: border-box;
-            h3{
-                text-align: justify ;
-                margin: 20px;
-                font-family: 'Poppins-Bold';
-                font-size: 21px;
-                color: #FEFEFE;
-            }
-            .internal-content{
-                p{
-                    margin: 10px;
-                }
-            }
-                    
-                   
-        }
-    }
-    .show-on-desktop{
-        display: none;
-    }
-
-    //Desktop
-    @media(min-width: 992px){
-        .show-on-desktop{
-        display: flex;
-        justify-content: space-between;
+        padding: 10px;
         .box{
+            margin: 20px;
             h3{
-                margin: 20px;
-                text-align: center;
-                color: #FEFEFE;
-                font-size: 25px;
-                font-family: 'Poppins-bold';
-                cursor: default;
-            }
-            h3:hover{
-                transform: translateX(10px) scale(1.2);
+                margin: 10px;
+                padding: 20px;
+                border-radius: 5px;
+                border: 3px solid #93B413;
+                background: #93B413;
+                height: 70px;
             }
             p{
                 margin: 20px;
-                width: 260px;
-                color: #FEFEFE;
-                font-family: 'Poppins-Regular';
-                font-size: 14px;
-                cursor: default;
+                width: 250px;
             }
-            p:hover{
-                transform: translateX(10px) scale(1.2);
+            a{
+                display: grid;
+                margin: 20px;
             }
-            span{
-                font-size: 15px;
-                font-family: 'Poppins-Semibold';
+            .container-icons{
+                margin: 20px;
             }
         }
-        a{
-            margin: 20px;
-            height: 31px;
-            display: grid;
-            font-family: 'Poppins-Regular';
-            font-size: 15px;
-            color: #FEFEFE;
-        }
-        a:hover{
-            transform: translateX(10px) scale(1.2);
-        }
-        .container{
-            height: 150px;
-            .icon-FI{
-                width : 100px ;
-                margin-left: auto;
-                margin-right: auto;
-                .facebook{
-                    width: 50px;
-                    height: 50px;
-                    fill: #3a77ff;
-                    cursor: pointer;
-                }
-                .facebook:hover{
-                    transform: translateX(10px) scale(1.2);
-                }
-                .instagram{
-                    width: 50px;
-                    height: 50px;
-                    fill:  #C82D8E;
-                    cursor: pointer;
-                }
-                .instagram:hover{
-                    transform: translateX(10px) scale(1.2);
-                }
-            }
-
-            .icon-TG{
-                width : 100px ;
-                margin-left: auto;
-                margin-right: auto;
-                .twitter{
-                    width: 50px;
-                    height: 50px;
-                    fill:  #098BEA;
-                    cursor: pointer;
-                }
-                .twitter:hover{
-                    transform: translateX(10px) scale(1.2);
-                }
-                .github{
-                    width: 50px;
-                    height: 50px;
-                    fill:  #000000;
-                    cursor: pointer;
-                }
-                .github:hover{
-                    transform: translateX(10px) scale(1.2);
-                }
-            }
-        
-        }
-        
+    }
+    //Desktop
+    @media(min-width: 992px){
+          
     }
     
-    .show-on-mobile{
-        display: none;
-    }
-    }
+    
+    
 
 `;
 
 const Footer = function (){
     return(
         <FooterComponent>
-            <nav className='show-on-mobile'>
-                <div className='container-center'>
-                    <div className='box-mobile'>
-                        <h3>Contatos</h3>
-                    </div>
-                    <div className='box-mobile'>
-                        <h3>Nossa Empresa</h3>
-                    </div>
-                    <div className='box-mobile'>
-                        <h3>Redes Sociais</h3>
-                    </div>
-                </div>
-            </nav>
-            <nav className='show-on-desktop'>
+            <div className='container-center'>
                 <div className='box'>
                     <h3>Contatos</h3>
                     <p><span>Endereço: </span>Rua Tamoios, 246, Jardim Aeroporto - São Paulo, SP.</p>
@@ -178,18 +58,15 @@ const Footer = function (){
                 </div>
                 <div className='box'>
                     <h3>Redes Sociais</h3>
-                    <div className='container'>
-                        <div className='icon-FI'>
-                            <FaFacebookSquare className='facebook'/>
-                            <FaInstagram className='instagram'/>
-                        </div>
-                        <div className='icon-TG'>
-                            <FaTwitterSquare className='twitter'/>
-                            <FaGithubSquare className='github'/>
-                        </div>
+                    <div className='container-icons'>
+                        <FaFacebookSquare className='facebook'/>
+                        <FaInstagram className='instagram'/>
+                        <FaTwitterSquare className='twitter'/>
+                        <FaGithubSquare className='github'/>
                     </div>
+                    
                 </div>
-            </nav>
+            </div>
         </FooterComponent>
     )
 }
