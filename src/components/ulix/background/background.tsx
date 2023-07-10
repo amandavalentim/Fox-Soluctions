b5b9e2ff9a3630c1c5f8ce71ed9d8696c67453a1
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-07-10 14:08:10 -0300

    Atualizando

--- src/components/ulix/background/background.tsx
+++ src/components/ulix/background/background.tsx
@@ -5,28 +5,26 @@ const BackgroundComponent = styled.div`
     background: linear-gradient(180deg, #07B3FD 0%, #0A53B5 100%);
     position: relative;
     background-repeat: no-repeat;
+    background-position: bottom;
     background-size: cover ;
     height: 100vh;
     width: 100%;
 
     @media (min-width: 768px){
     background-image: url("/images/background-tablet-2.svg");
-    background-position-y: -85px;
     }
 
     @media (min-width: 992px){
     background-image: url("/images/background3.svg");
-    background-position-y: -100px;
     }
 
     @media (min-width: 1200px){
-    background-image: url("/images/background-2.svg");
-    background-position-y: -110px;
+    background-image: url("/images/background-2.svg"); 
+    background-position: bottom;
     }
 
     @media (min-width: 1600px) {
         background-image: url("/images/background-tall-screens.svg");
-        background-position-y: -100px;
     }
 `;
 
