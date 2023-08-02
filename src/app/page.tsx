b731788a0535533501823f6a4d4d8b57c7f46aa6
Amanda Vitoria Valentim Dos Anjos <amanda.anjos@tecno.com.br>
2023-08-02 15:12:03 -0300

    realizando atualizações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import Background from '@/components/ulix/background/background'
-<<<<<<< HEAD
-
-import Form from '../components/Forms/Form'
-=======
-import Head  from '../components/Head/Head'
-import Footer  from '../components/Footer/Footer'
-import Form from '@/components/Forms'
->>>>>>> eede5d9febb82633c51ae0281e5aadc6a0bdd3ef
+import Form from '../components/Forms'
 
 export default function Home() {
   return (
