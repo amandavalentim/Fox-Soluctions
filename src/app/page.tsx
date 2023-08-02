2538b478567092b7f3542a9a5390a19eea4401f7
Gabriel Marques Correia <gabrielmarquescorreia4@gmail.com>
2023-08-02 15:11:37 -0300

    Atualizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import Background from '@/components/ulix/background/background'
-<<<<<<< HEAD
-
-import Form from '../components/Forms/Form'
-=======
 import Head  from '../components/Head/Head'
 import Footer  from '../components/Footer/Footer'
 import Form from '@/components/Forms'
->>>>>>> eede5d9febb82633c51ae0281e5aadc6a0bdd3ef
 
 export default function Home() {
   return (
