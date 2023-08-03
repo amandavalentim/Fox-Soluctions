7b98449cf1f8e6519ddb72d77da871aba1b81158
Amanda Vitoria Valentim Dos Anjos <amanda.anjos@tecno.com.br>
2023-08-03 10:10:59 -0300

    atualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Background from '@/components/ulix/background/background'
-<<<<<<< HEAD
-import Form from '@/components/Forms'
-=======
 import Form from '../components/Forms'
->>>>>>> 5eb9b6150c7dfe72e5742464879d0c95fcc6e33c
 
 export default function Home() {
   return (
---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,17 @@
+'use client';
+import styled from '@emotion/styled';
+import React, { useState } from 'react';
+
+const AboutUsComponent = styled.section`
+
+    
+
+`;
+
+const AboutUs = function (){
+
+    <AboutUsComponent></AboutUsComponent>
+
+}
+
+export default AboutUs;
