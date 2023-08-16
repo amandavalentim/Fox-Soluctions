fa2089e4b3025ed307a72c116b349ad091db6645
Amanda <amandavitoriavalentim04@gmail.com>
2023-08-16 12:35:00 -0300

    salvando alterações realizadas

--- src/app/AboutUs/dist/page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AboutUs_1 = require("../../components/AboutUs/AboutUs");
-function Home() {
-    return (React.createElement("main", null,
-        React.createElement(AboutUs_1["default"], null)));
-}
-exports["default"] = Home;
--- src/app/AboutUs/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutUs from '../../components/AboutUs/AboutUs'
-
-export default function Home() {
-  return (
-    <main>
-      <AboutUs/>
-    </main>
-  )
-}
