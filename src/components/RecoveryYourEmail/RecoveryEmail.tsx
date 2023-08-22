3e34b4425f74599fe5d36a458526dd61f91ba7b0
Amanda <amandavitoriavalentim04@gmail.com>
2023-08-22 10:41:15 -0300

    finalizado seção recuperar email mobile

--- src/components/RecoveryYourEmail/RecoveryEmail.tsx
+++ src/components/RecoveryYourEmail/RecoveryEmail.tsx
@@ -2,13 +2,53 @@
 
 'use client';
 import styled from '@emotion/styled';
+import Input from '../Login/Input/Input';
 
-const RecoveryEmailComponent = styled.section``
+const RecoveryEmailComponent = styled.section`
+    height: 70vh;
+    background: linear-gradient(180deg, #04a4e8 0%, #0A53B5 100%);
+    .mobile{
+        h3{
+            padding: 40px;
+            text-align: center;
+            color: #ffffff;
+            font-family:'Poppins-Bold';
+        }
+        #PhoneOrCPF{
+            margin: 50px;
+            padding: 4px;
+        }
+        .btn-confirm{
+            text-align: center;
+            .confirm{
+                width: 170px;
+                height: 50px;
+                border-radius: 20px;
+                border: none;
+                background: #93B413;
+                font-size: 1.6em;
+                color: #ffffff;
+                margin: 40px;
+            }
+        }
+    }
+`
 
 const RecoveryEmail = function (){
     return(
         <RecoveryEmailComponent>
+            <nav className='mobile'>
+                <h3>Recupere Seu Email:</h3>
+                <Input type="string" name="PhoneOrCPF" id="PhoneOrCPF" placeholder='Digite Seu Telefone ou CPF'/>
+                <div className='btn-confirm'>
+                    <button className='confirm' type='button'>
+                        Enviar
+                    </button>
+                </div>
+            </nav>
 
+            <nav className='desktop'>
+            </nav>
         </RecoveryEmailComponent>
     )
 }
