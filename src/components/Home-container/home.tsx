5a43bbba11d3f2f984030e861e0e2f080a8e67f7
Gabriel <gabrielmarquescorreia4@gmail.com>
2023-11-08 14:27:51 -0300

    main

--- src/components/Home-container/home.tsx
+++ src/components/Home-container/home.tsx
@@ -353,7 +353,7 @@ const PgInitial = function() {
 
             <nav className='mobile'>
                 {/* Content */}
-                    <h1>Bem-Vinde a<br/> <span id='FX'>Fox Solutions</span>!</h1>
+                    <h1>Bem-Vindos a<br/> <span id='FX'>Fox Solutions</span>!</h1>
                     <p>
                         Aqui nós buscamos sempre<br/> dar a melhor<br/> experiência possível para<br/> quem utiliza nossa<br/> plataforma, onde você pode<br/> realizar o upload e visualizar<br/> seus documentos!
                     </p>
@@ -367,7 +367,7 @@ const PgInitial = function() {
 
             <nav className='tablet-desktops'>
                 {/* Content */}
-                <h1>Bem-Vinde a<br/> <span id='FX'>Fox Solutions</span>!</h1>
+                <h1>Bem-Vindos a<br/> <span id='FX'>Fox Solutions</span>!</h1>
                 <p>
                      Aqui nós buscamos sempre dar a melhor<br/> experiência possível para quem utiliza nossa<br/> plataforma, onde você pode realizar o upload<br/> e visualizar seus documentos!
                 </p>
